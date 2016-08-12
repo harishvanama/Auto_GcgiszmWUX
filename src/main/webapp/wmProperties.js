ec26d78d6137d8798506fb6a93c844333d9ba122
root <root@338311f47977>
2016-08-12 05:23:37 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_GcgiszmWUX",
+  "homePage" : "Main",
+  "name" : "Auto_GcgiszmWUX",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
